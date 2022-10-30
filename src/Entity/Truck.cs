a70858a85629cf83d2468931da180e71b492241d
Mazzei64 <mazzei.ws@gmail.com>
2022-10-30 19:22:32 -0300

    feature: introduces truck entity.

---
+++ src/Entity/Truck.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TruckGarage.Entity {
+    public class Truck {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string modelo { get; set; } = string.Empty;
+        [Required]
+        public DateTime anoFabricacao { get; set; }
+        [Required]
+        public DateTime anoModelo { get; set; }
+    }
+}
